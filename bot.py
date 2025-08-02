5cdef9f243fce9f5be2deaa78fd5495da16169e3
Faeze2024 <zmollaei.2024@gmail.com>
2025-08-02 21:33:30 +0330

    add runtime.txt and change the code.

--- bot.py
+++ bot.py
@@ -2,16 +2,13 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Successfully run in Render ✅")
-
-# توکن رو از متغیر محیطی BOT_TOKEN می‌گیره
 TOKEN = os.getenv("BOT_TOKEN")
-
 if not TOKEN:
     raise ValueError("❌ BOT_TOKEN environment variable is not set!")
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Successfully run in Render ✅")
+
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-
 app.run_polling()
